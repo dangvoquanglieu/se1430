bcb24ea5ab2b92df72d43f092605da849ad47f1a
dangvoquanglieu <lieuhocgioisomot@gmail.com>
2020-11-14 12:12:26 +0700

    2nd: import modification

--- src/se1430/Se1430.java
+++ src/se1430/Se1430.java
@@ -5,7 +5,9 @@
  */
 package se1430;
 
-import lieu.util.MathUtility;
+import static lieu.util.MathUtility.getFactorial;
+import static lieu.util.MathUtility.PI;
+//câu lệnh mới từ JDK 8 (5), khai báo static ko cần chám, chỉ áp dụn
 
 /**
  *
@@ -20,17 +22,20 @@ public class Se1430 {
         //ta test thử hàm của ta
         //dùng mắt để so sánh kết quả thực tế khi chạy và kết quả mong đợi
         long expected = 120;//ta mong nhận được 120 nếu gọi hàm 5!
-        long actual = MathUtility.getFactorial(5);
+        long actual = getFactorial(5);
         System.out.println("5!: Expected: " + expected + " Actual " + actual);
-        
+
         //expected: 720 nếu if tính 6!
-        System.out.println("6!: " + MathUtility.getFactorial(6));
-        
+        System.out.println("6!: " + getFactorial(6));
+
         //expected: 720 nếu if tính 0!
-        System.out.println("0!: " + MathUtility.getFactorial(0));
-        
+        System.out.println("0!: " + getFactorial(0));
+
         //expected: IllegalArgumentException if tính -5
-        System.out.println("-5: " + MathUtility.getFactorial(-5));
+        System.out.println("-5: " + getFactorial(-5));
+
+        //in thử PI có giống bên Math.PI không
+        System.out.println("PI: " + PI);
     }
 
 }
